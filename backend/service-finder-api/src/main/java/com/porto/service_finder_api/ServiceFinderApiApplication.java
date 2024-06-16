1915377ca928e345df3f964136b6cb2aeeb2c265
Vinícius Porto <leinateporto@gmail.com>
2024-06-15 21:00:40 -0300

    feat: commit inicial

---
+++ backend/service-finder-api/src/main/java/com/porto/service_finder_api/ServiceFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.porto.service_finder_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFinderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceFinderApiApplication.class, args);
+	}
+
+}
---
+++ backend/service-finder-api/src/test/java/com/porto/service_finder_api/ServiceFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porto.service_finder_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFinderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
